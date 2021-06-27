5820f83be69bb118a1de66ae4cfc9be05a1f4fa0
elwillan <williamgarnica@ingenieros.com>
2021-06-27 18:54:09 -0500

    se agrega rest home, para ruta base

---
+++ src/main/java/pruebaingeneo/restservices/HomeREST.java
@@ -0,0 +1,23 @@
+package pruebaingeneo.restservices;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin
+public class HomeREST {
+
+	@Autowired
+
+	@GetMapping("/")
+	public String home() {
+		return "Servidor BackEnd para prueba de Ingeneo. Servidor arriba. Hora servidor: " + new Date();
+	}
+
+}
